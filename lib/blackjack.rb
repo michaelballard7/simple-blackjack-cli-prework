447e708e4a879b2563e83977302f34047df38b99
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 17:32:24 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(number)
   prompt_user
   input = get_user_input
   if input == 's'
-    initial_round
+    display_card_total
   elsif input == 'h'
     current_total = initial_round
     new_card = deal_card
